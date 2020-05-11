2947b6667a90225f29a34dc8d71bbde3675f4e18
MohamedEshmawy <36339453+MohamedEshmawy@users.noreply.github.com>
2020-05-11 23:54:12 +0200

    Added siammask model

---
+++ core/siammask/utils/good_ones.py
@@ -0,0 +1,58 @@
+def get_subwindow_tracking(im, pos, model_sz, original_sz, avg_chans, out_mode='torch'):
+    '''
+    Crops the image around a point and if the crop is out of edge of the image, it
+    fills it with a fill color.
+    params:
+        im: np.array, image to be cropped
+        pos: np.array, center of the crop
+        model_sz: int, output size of the crop after resizing
+        original_sz: int, size of the crop in the image
+        avg_channels: np.array, fill color used to fill crop when it goes out of bounds
+        out_mode: string, return image as numpy or tensor torch
+    example: z = get_subwindow_tracking(im, np.array([375. , 138.5]), 127, 100,
+            avg_chans = np.array([ 96, 114, 141]), out_mode='torch')
+    '''
+    if isinstance(pos, float):
+        pos = [pos, pos]
+    sz = original_sz
+    im_sz = im.shape
+    c = (original_sz + 1) / 2
+    context_xmin = round(pos[0] - c)
+    context_xmax = context_xmin + sz - 1
+    context_ymin = round(pos[1] - c)
+    context_ymax = context_ymin + sz - 1
+    left_pad = int(max(0., -context_xmin))
+    top_pad = int(max(0., -context_ymin))
+    right_pad = int(max(0., context_xmax - im_sz[1] + 1))
+    bottom_pad = int(max(0., context_ymax - im_sz[0] + 1))
+
+    context_xmin = context_xmin + left_pad
+    context_xmax = context_xmax + left_pad
+    context_ymin = context_ymin + top_pad
+    context_ymax = context_ymax + top_pad
+
+    # zzp: a more easy speed version
+    r, c, k = im.shape
+    if any([top_pad, bottom_pad, left_pad, right_pad]):
+        te_im = np.zeros((r + top_pad + bottom_pad, c + left_pad + right_pad, k), np.uint8)
+        te_im[top_pad:top_pad + r, left_pad:left_pad + c, :] = im
+        if top_pad:
+            te_im[0:top_pad, left_pad:left_pad + c, :] = avg_chans
+        if bottom_pad:
+            te_im[r + top_pad:, left_pad:left_pad + c, :] = avg_chans
+        if left_pad:
+            te_im[:, 0:left_pad, :] = avg_chans
+        if right_pad:
+            te_im[:, c + left_pad:, :] = avg_chans
+        im_patch_original = te_im[int(context_ymin):int(context_ymax + 1), int(context_xmin):int(context_xmax + 1), :]
+    else:
+        im_patch_original = im[int(context_ymin):int(context_ymax + 1), int(context_xmin):int(context_xmax + 1), :]
+
+    if not np.array_equal(model_sz, original_sz):
+        im_patch = cv2.resize(im_patch_original, (model_sz, model_sz))
+    else:
+        im_patch = im_patch_original
+    # cv2.imshow('crop', im_patch)
+    # cv2.waitKey(0)
+    return im_to_torch(im_patch) if out_mode in 'torch' else im_patch
+
